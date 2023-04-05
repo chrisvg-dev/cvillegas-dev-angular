5cb1015883587a3488d1d0651e538f3155a4e7ee
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-04-05 15:08:36 -0600

    Projects added

--- src/app/components/sugestions/sugestions.component.ts
+++ src/app/components/sugestions/sugestions.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sugestions',
   templateUrl: './sugestions.component.html',
   styleUrls: ['./sugestions.component.css']
 })
-export class SugestionsComponent {
+export class SugestionsComponent implements OnInit {
 
-}
+  constructor(private http: HttpClient){}
+  
+  ngOnInit(): void {
+    this.http.get('http://cvillegas-dev.com:9191/api/v1/projects').subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
+
+} 
